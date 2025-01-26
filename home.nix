7fb757a9ccc65069cc24ee905ad86177b0b8b952
Sebastian Ritz <git@rtz.cx>
2025-01-26 19:25:26 +0100

    Set 144Hz refresh rate GNOME

--- home.nix
+++ home.nix
@@ -49,6 +49,34 @@
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
+    
+    # Set refresh rate to 144Hz
+    ".config/monitors.xml".text = ''
+<monitors version="2">
+  <configuration>
+    <layoutmode>physical</layoutmode>
+    <logicalmonitor>
+      <x>0</x>
+      <y>0</y>
+      <scale>1</scale>
+      <primary>yes</primary>
+      <monitor>
+        <monitorspec>
+          <connector>DP-1</connector>
+          <vendor>XMI</vendor>
+          <product>Mi Monitor</product>
+          <serial>0x00000000</serial>
+        </monitorspec>
+        <mode>
+          <width>3440</width>
+          <height>1440</height>
+          <rate>144.000</rate>
+        </mode>
+      </monitor>
+    </logicalmonitor>
+  </configuration>
+</monitors>
+    '';
   };
 
   # Home Manager can also manage your environment variables through
