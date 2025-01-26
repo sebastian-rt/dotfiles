0a8a7f245cc93f9e248ae032e89e80bff39010d2
Sebastian Ritz <git@rtz.cx>
2025-01-26 19:15:40 +0100

    Add home-manager module

--- flake.nix
+++ flake.nix
@@ -5,9 +5,13 @@
     # NixOS official package source, using the nixos-24.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     impermanence.url = "github:nix-community/impermanence";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, impermanence, ... }@inputs: {
+  outputs = { self, nixpkgs, impermanence, home-manager, ... }@inputs: {
     nixosConfigurations.nixstation = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -15,6 +19,14 @@
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
         ./configuration.nix
+        
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          
+          home-manager.users.sebastian = import ./home.nix;
+        }
       ];
     };
   };
