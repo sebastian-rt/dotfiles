cf321db14fefa36d2fbc803d9693d65e159d1b6d
Sebastian Ritz <git@rtz.cx>
2025-01-27 21:43:10 +0100

    user: Disable "Welcome to GNOME" dialog

--- user/gnome.nix
+++ user/gnome.nix
@@ -11,6 +11,9 @@
       "org/gnome/desktop/background".picture-uri-dark = "file:///run/current-system/sw/share/backgrounds/gnome/amber-d.jxl";
       "org/gnome/desktop/background".primary-color = "#ff7800";
       "org/gnome/desktop/background".secondary-color = "#000000";
+
+      # Disable "Welcome to GNOME" dialog
+      "org/gnome/shell".welcome-dialog-last-shown-version = "999"; # A huge number represents a version that doesn't yet exist, effectively disabling the dialog
     };
   };
 }
