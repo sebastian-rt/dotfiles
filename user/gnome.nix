210f9fbd068c5d062b43e0e397757a2f98fa67d1
Sebastian Ritz <git@rtz.cx>
2025-01-27 18:26:01 +0100

    user: GNOME `Amber` wallpaper

--- user/gnome.nix
+++ user/gnome.nix
@@ -5,6 +5,12 @@
     dconf.settings = {
       # Set dark mode
       "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+
+      # GNOME "Amber" wallpaper
+      "org/gnome/desktop/background".picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/amber-l.jxl";
+      "org/gnome/desktop/background".picture-uri-dark = "file:///run/current-system/sw/share/backgrounds/gnome/amber-d.jxl";
+      "org/gnome/desktop/background".primary-color = "#ff7800";
+      "org/gnome/desktop/background".secondary-color = "#000000";
     };
   };
 }
