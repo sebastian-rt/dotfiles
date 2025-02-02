3204cdba1d09d0f0276874b955544f1decea80ec
Sebastian Ritz <git@rtz.cx>
2025-02-02 15:55:31 +0100

    system: debloat GNOME

--- system/default.nix
+++ system/default.nix
@@ -7,6 +7,15 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.excludePackages = [pkgs.xterm]; # Exclude xterm
+
+  # Exclude GNOME packages I don't need
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-tour
+    yelp
+    gnome-console
+    snapshot
+  ];
 
   # Keyring
   services.gnome.gnome-keyring.enable = true;
