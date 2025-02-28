12e3081b119eb1d14d54830cc97c729e3a84f03c
Sebastian Ritz <git@rtz.cx>
2025-02-28 12:01:50 +0100

    user/vscode: Force `gnome-keyring`

--- user/programs/development/vscode.nix
+++ user/programs/development/vscode.nix
@@ -1,29 +1,38 @@
 {pkgs, ...}: {
-  home-manager.users.sebastian.programs.vscode = {
-    enable = true;
-    package = pkgs.vscode;
-    extensions = with pkgs.vscode-extensions; [
-      jnoortheen.nix-ide
-      ms-python.python
-      github.copilot
-      github.copilot-chat
-    ];
-    userSettings = {
+  home-manager.users.sebastian = {
+    programs.vscode = {
+      enable = true;
+      package = pkgs.vscode;
+      extensions = with pkgs.vscode-extensions; [
+        jnoortheen.nix-ide
+        ms-python.python
+        github.copilot
+        github.copilot-chat
+      ];
+      userSettings = {
         "telemetry.telemetryLevel" = "off";
-      "window.titleBarStyle" = "custom"; # Use vscode window decorations
-      "editor.formatOnSave" = true;
-      "security.workspace.trust.enabled" = false;
-      # Nix IDE Extension
-      "nix.serverPath" = "nixd";
-      "nix.enableLanguageServer" = true;
-      "nix.serverSettings" = {
-        "nixd" = {
-          "formatting" = {
-            "command" = ["alejandra"];
+        "window.titleBarStyle" = "custom"; # Use vscode window decorations
+        "editor.formatOnSave" = true;
+        "security.workspace.trust.enabled" = false;
+        # Nix IDE Extension
+        "nix.serverPath" = "nixd";
+        "nix.enableLanguageServer" = true;
+        "nix.serverSettings" = {
+          "nixd" = {
+            "formatting" = {
+              "command" = ["alejandra"];
+            };
           };
         };
       };
     };
+
+    home.file.".vscode/argv.json".text = ''
+      {
+        "enable-crash-reporter": false,
+        "password-store": "gnome-libsecret", // Force gnome-keyring; Required for window managers other than GNOME
+      }
+    '';
   };
 
   environment.persistence."/persist".users.sebastian.directories = [".config/Code"];
