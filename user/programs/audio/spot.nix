6ff8cbfecf92e8c48c1280e511442100d05c960f
Sebastian Ritz <git@rtz.cx>
2025-03-29 01:17:20 +0100

    user/spot: Apply patch to fix playlist display issue

--- user/programs/audio/spot.nix
+++ user/programs/audio/spot.nix
@@ -1,6 +1,18 @@
 {pkgs, ...}: {
   home-manager.users.sebastian.home.packages = with pkgs; [
-    spot
+    # Here we patch the spot package to fix a bug that prevents playlists without a user added image from showing up
+    # See here: https://github.com/xou816/spot/pull/716
+    (spot.overrideAttrs
+      (oldAttrs: {
+        version = "0.5.0-fix";
+
+        patches = [
+          (fetchpatch {
+            url = "https://github.com/xou816/spot/commit/cd55228b282c4728368fa42fe2848ab8dfb8ee1a.patch";
+            hash = "sha256-5F9InR8uh5e7bGhwGkw0pQZZ62GrapVIPpauz7lKPUM=";
+          })
+        ];
+      }))
   ];
 
   environment.persistence."/persist".users.sebastian.directories = [".cache/spot"];
