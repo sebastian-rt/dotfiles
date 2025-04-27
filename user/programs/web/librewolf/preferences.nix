837f53fa70257f6551f0c435bddc99357c3bbfb5
Sebastian Ritz <git@rtz.cx>
2025-04-27 23:13:16 +0200

    user/librewolf: Force hardware video decoding

--- user/programs/web/librewolf/preferences.nix
+++ user/programs/web/librewolf/preferences.nix
@@ -193,6 +193,7 @@
   # VAAPI
   "media.ffmpeg.vaapi.enabled" = true;
   "media.rdd-ffmpeg.enabled" = true;
+  "media.hardware-video-decoding.force-enabled" = true;
   "media.av1.enabled" = true;
   "gfx.x11-egl.force-enabled" = true;
   "widget.dmabuf.force-enabled" = true;
